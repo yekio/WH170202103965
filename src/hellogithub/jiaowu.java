7106d7cb8c7a2ad6bf90a03a13f2bd55aa622f62
Administrator <Administrator@MB405-38>
2019-05-14 08:50:45 +0800

    MyEclipse提交

---
+++ src/hellogithub/jiaowu.java
@@ -0,0 +1,7 @@
+package hellogithub;
+
+public class jiaowu {
+	public static void main(String[] args) {
+		System.out.println("good!");
+	}
+}
---
+++ src/hellogithub/student.java
@@ -0,0 +1,11 @@
+package hellogithub;
+
+public class student {
+	   public String name;
+	   public String address;
+	   
+	   student(){
+		   
+	   }
+	}
+
